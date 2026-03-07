3e5f864aca501362f5674b76521c60184f6f648a
parthsinh45-tech <parthsinh45@gmail.com>
2026-03-07 19:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function scrollSection(){
+function generateResume(){
 
-document.getElementById("ai").scrollIntoView({
-behavior:"smooth"
-});
+let name=document.getElementById("name").value;
+let role=document.getElementById("role").value;
+let skills=document.getElementById("skills").value;
+let experience=document.getElementById("experience").value;
+
+let resume=`
+
+<h2>${name}</h2>
+
+<h3>${role}</h3>
+
+<h4>Skills</h4>
+
+<p>${skills}</p>
+
+<h4>Experience</h4>
+
+<p>${experience}</p>
+
+`;
+
+document.getElementById("resumePreview").innerHTML=resume;
+
+}
+
+
+
+function downloadPDF(){
+
+window.print();
 
 }
