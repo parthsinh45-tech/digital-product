f9f3277b94b2306e6e6ac2b002e68e6e2a975862
parthsinh45-tech <parthsinh45@gmail.com>
2026-03-07 15:39:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function scrollToEbooks(){
+function payNow(){
 
-document.getElementById("ebooks").scrollIntoView({
-behavior:"smooth"
-});
+var options = {
+
+"key": "YOUR_RAZORPAY_KEY",
+
+"amount": 4900,
+
+"currency": "INR",
+
+"name": "PikaTech45",
+
+"description": "Cricket Ebook",
+
+"handler": function (response){
+
+window.location.href="success.html";
+
+}
+
+};
+
+var rzp = new Razorpay(options);
+
+rzp.open();
 
 }
