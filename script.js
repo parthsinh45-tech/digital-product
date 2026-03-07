e4a7d0433ee68d4165d3f95aba8c8a766e9956d7
parthsinh45-tech <parthsinh45@gmail.com>
2026-03-07 19:46:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-async function payNow(){
+function scrollSection(){
 
-const res = await fetch("/create-order",{method:"POST"});
-const order = await res.json();
-
-var options = {
-
-key:"rzp_live_SOJbS3S4m8sNI3",
-
-amount:order.amount,
-
-currency:"INR",
-
-order_id:order.id,
-
-handler:async function(response){
-
-const verify = await fetch("/verify-payment",{
-
-method:"POST",
-
-headers:{"Content-Type":"application/json"},
-
-body:JSON.stringify(response)
-
+document.getElementById("ai").scrollIntoView({
+behavior:"smooth"
 });
-
-const data = await verify.json();
-
-if(data.success){
-
-window.location=data.download;
-
-}
-
-}
-
-};
-
-var rzp=new Razorpay(options);
-
-rzp.open();
 
 }
